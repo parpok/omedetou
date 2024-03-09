7cdfda3e439f9637d42a74b3ec30ac15648469f5
Patryk <me@parpok.xyz>
2024-03-09 14:09:57 +0100

    Complete the backend

--- omedetouBackend/index.js
+++ omedetouBackend/index.js
@@ -1,17 +1,35 @@
 // I forgot I was trying to make a backend in PHP
 // Whatever maybe express JS will be easier for a basic side project
 
-// RN Ill leave hello world
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
+const port = 3000;
+class Item {
+  constructor(description, id = crypto.randomUUID()) {
+    this.id = id;
+    this.description = description;
+  }
+}
 
-const express = require('express')
-const app = express()
-const port = 3000
+let affirmations = [
+  "おめでとう",
+  "おめでたいな",
+  "Congratulations",
+  "Congrats",
+  "Gratulacje",
+  "Gratulacje",
+];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let items = affirmations.map(
+  (affirmation) => new Item(affirmation)
+);
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(items));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
